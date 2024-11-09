262c805f09923995820166a7633defa130864c4e
Uliana Benato <ulianabenato@gmail.com>
2024-11-09 16:20:36 -0300

    Trabalho
    Signed-off-by: Uliana Benato <ulianabenato@gmail.com>

--- app/src/main/java/com/example/carApi/services/RetrofitClient.kt
+++ app/src/main/java/com/example/carApi/services/RetrofitClient.kt
@@ -5,21 +5,13 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
-    private const val BASE_URL = "http://192.168.1.16:3000/" //10.0.2.2 ou ip 192.168.1.45
-    //É o endereço usado para acessar o localhost no emulador android - arrumar no xml tb - arquivo de permissão
-
-    /*private val loggingInterceptor = HttpLoggingInterceptor().apply {
-        level = HttpLoggingInterceptor.Level.BODY
-    }
-    private val okHttpClient = OkHttpClient.Builder()
-        .addInterceptor(loggingInterceptor)
-        //.addInterceptor(GeoLocationInterceptor(DatabaseBuilder.getInstance().userLocationDoa()))
-        .build()*/
+    private const val BASE_URL = "http://10.0.2.2:3000/"
+    //Professor Vagner, eu usei meu IP aqui na minha
+    // máquina porque executo o app no celular, mas deixei 10.0.2.2 para você executar aí
 
     private val instance: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
-            //.client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create()) // converte o json para o firebase
             .build()
     }
